090b3fe51cf51a22d67d054124de61bcceb64a9f
zcemdki <36973437+zcemdki@users.noreply.github.com>
2019-04-01 02:27:40 +0100

    Add files via upload

---
+++ INCOMPLETE/upload_image.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="../BACKEND/includes_upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+
+</body>
+</html>
